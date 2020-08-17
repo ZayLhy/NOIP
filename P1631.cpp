27e964de0f4058080bf11727c17dd77d45ab4f82
lwzhenglittle <924695205@qq.com>
2020-08-17 17:07:51 +0800

    1631

--- P1631.cpp
+++ P1631.cpp
@@ -21,14 +21,37 @@ int main()
     {
         scanf("%d", &y[i]);
     }
-    sort(a + 1, a + n + 1);
-    sort(b + 1, b + n + 1);
+    sort(x + 1, x + n + 1);
+    sort(y + 1, y + n + 1);
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            
+            if (a.size() < n)
+            {
+                a.push((x[i] + y[j]));
+                continue;
+            }
+            if ((x[i] + y[j]) < (a.top()))
+            {
+                a.pop();
+                a.push((x[i] + y[j]));
+                continue;
+            }
+            break;
         }
     }
+    vector<int> b;
+    for (int i = 1; i <= n; i++)
+    {
+        b.push_back(a.top());
+        a.pop();
+    }
+    reverse(b.begin(), b.end());
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", b[i - 1]);
+    }
+    cout << endl;
     return 0;
 }
