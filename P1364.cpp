42cf8a04e962fac94a09d299596059af053d6640
lwzhenglittle <924695205@qq.com>
2020-09-05 10:06:40 +0800

    little changes

---
+++ P1364.cpp
@@ -0,0 +1,18 @@
+/*********************************************************************
+    程序名: P1364
+    版权: MIT License
+    作者: lwzheng
+    日期: 2020-09-05 10:03
+    说明: Luogu P1364
+*********************************************************************/
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
