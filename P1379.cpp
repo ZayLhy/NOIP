192697346d673c203bb2ad2e57bdfdb31fb2fb0a
lwzhenglittle <924695205@qq.com>
2020-09-19 00:12:41 +0000

    P1379

--- P1379.cpp
+++ P1379.cpp
@@ -5,17 +5,36 @@
 #include<string>
 #include<queue>
 #include<unordered_map>
+#include<pair>
 using namespace std;
-unordered_map<int,int> a;
+unordered_map<int,int> m,ans;
 const long long goal = 123804765;
 const int mx[] = {1,-1,0,0};
 const int my[] = {0,0,1,-1};
+void transform(int* a,long long status){
+    for(int i=3;i>=1;i--){
+        for(int j=3;j>=1;j--){
+            a[i][j]=status%10;
+            status/=10;
+            if(a[i][j]==0){
+                pair<int,int> pos = make_pair(i,j);
+            }
+        }
+    }
+    return pos;
+}
 void bfs(long long status){
+    queue<long long> Q;
     if(status==goal){
         printf("0\n");
         return ;
     }
-    
+    int tmp[5][5];
+    Q.push(status);
+    Q.push(goal);
+    while(!Q.empty()){
+
+    }
 }
 int main()
 {
