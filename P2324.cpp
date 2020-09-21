9be61b1fe2457178ccff3b85279c5493ef5608ef
lwzhenglittle <924695205@qq.com>
2020-09-21 02:50:02 +0000

    2324 init

---
+++ P2324.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+void work(){
+    return ;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        work();
+    }
+    return 0;
+}
